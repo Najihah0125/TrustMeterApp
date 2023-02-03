90d05c4dadd387324b380d3006c56b9dad74f64b
Nur Najihah <nurnajihah.mrizal@petronas.com>
2023-02-03 21:58:06 +0800

    Create bottom navigation bar

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
-
+import 'package:trustmeter/Screens/history_screen.dart';
 import '../Functions/search_function.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -12,28 +10,18 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  // int _selectedIndex = 0;
+  int _selectedIndex = 0;
 
-  // static const List<Widget> _pages = <Widget>[
-  //   Icon(
-  //     Icons.call,
-  //     size: 150,
-  //   ),
-  //   Icon(
-  //     Icons.camera,
-  //     size: 150,
-  //   ),
-  //   Icon(
-  //     Icons.chat,
-  //     size: 150,
-  //   ),
-  // ];
+  static const List<Widget> _pages = <Widget>[
+    SearchFunction(),
+    HistoryScreen(),
+  ];
 
-  // void _onItemTapped(int index) {
-  //   setState(() {
-  //     _selectedIndex = index;
-  //   });
-  // }
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -48,37 +36,33 @@ class _HomeScreenState extends State<HomeScreen> {
         actions: <Widget>[
           TextButton.icon(
             onPressed: () {},
-            icon: Icon(
+            icon: const Icon(
               Icons.logout,
               size: 24.0,
               color: Colors.white,
             ),
-            label: Text(
+            label: const Text(
               'Logout',
               style: TextStyle(color: Colors.white),
             ),
           ),
         ],
       ),
-      // bottomNavigationBar: BottomNavigationBar(
-      //   iconSize: 30,
-      //   selectedFontSize: 16,
-      //   unselectedFontSize: 16,
-      //   backgroundColor: const Color.fromARGB(255, 135, 176, 255),
-      //   items: const <BottomNavigationBarItem>[
-      //     BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-      //     BottomNavigationBarItem(
-      //         icon: Icon(Icons.history), label: "History"),
-      //   ],
-      //   currentIndex: _selectedIndex,
-      //   selectedItemColor: const Color.fromARGB(255, 43, 115, 255),
-      //   selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
-      //   onTap: _onItemTapped,
-      // ),
-      // body: Center(
-      //   child: _pages.elementAt(_selectedIndex),
-      // )
-      body: SearchFunction(),
+      bottomNavigationBar: BottomNavigationBar(
+        iconSize: 30,
+        selectedFontSize: 16,
+        unselectedFontSize: 16,
+        backgroundColor: const Color.fromARGB(255, 135, 176, 255),
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.history), label: "History"),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: const Color.fromARGB(255, 43, 115, 255),
+        selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
+        onTap: _onItemTapped,
+      ),
+      body: _pages.elementAt(_selectedIndex),
     );
   }
 }
