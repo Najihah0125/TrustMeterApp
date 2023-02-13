26c9dd4159711b272649ddaea2773392e7de9361
Nur Najihah <nurnajihah.mrizal@petronas.com>
2023-02-13 22:45:05 +0800

    Delete unused file

--- lib/Functions/backup.dart
+++ lib/Functions/backup.dart
@@ -1,47 +1,47 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:trustmeter/Components/text_field_component.dart';
-import 'package:dropdown_search/dropdown_search.dart';
-import 'package:trustmeter/Models/Seller.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter/src/widgets/container.dart';
+// import 'package:flutter/src/widgets/framework.dart';
+// import 'package:trustmeter/Components/text_field_component.dart';
+// import 'package:dropdown_search/dropdown_search.dart';
+// import 'package:trustmeter/Models/Seller.dart';
 
-class SearchFunction extends StatefulWidget {
-  const SearchFunction({super.key});
+// class SearchFunction extends StatefulWidget {
+//   const SearchFunction({super.key});
 
-  @override
-  State<SearchFunction> createState() => _SearchFunctionState();
-}
+//   @override
+//   State<SearchFunction> createState() => _SearchFunctionState();
+// }
 
-class _SearchFunctionState extends State<SearchFunction> {
-  String name = "";
+// class _SearchFunctionState extends State<SearchFunction> {
+//   String name = "";
 
-  @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return StreamBuilder<QuerySnapshot>(
-        stream: FirebaseFirestore.instance.collection('sellers').snapshots(),
-        builder: (context, snapshots) {
-          return (snapshots.connectionState == ConnectionState.waiting)
-              ? Center(
-                  child: CircularProgressIndicator(),
-                )
-              : DropdownSearch<String>(
-                  mode: Mode.MENU,
-                  items: snapshots.data!.docs
-                      .map((DocumentSnapshot document) {
-                        Map<String, dynamic> data =
-                            document.data()! as Map<String, dynamic>;
-                        return data["seller_name"];
-                      })
-                      .toList()
-                      .cast<String>(),
-                  showSelectedItems: true,
-                  showSearchBox: true,
-                  onChanged: print,
-                );
+//   @override
+//   Widget build(BuildContext context) {
+//     Size size = MediaQuery.of(context).size;
+//     return StreamBuilder<QuerySnapshot>(
+//         stream: FirebaseFirestore.instance.collection('sellers').snapshots(),
+//         builder: (context, snapshots) {
+//           return (snapshots.connectionState == ConnectionState.waiting)
+//               ? Center(
+//                   child: CircularProgressIndicator(),
+//                 )
+//               : DropdownSearch<String>(
+//                   mode: Mode.MENU,
+//                   items: snapshots.data!.docs
+//                       .map((DocumentSnapshot document) {
+//                         Map<String, dynamic> data =
+//                             document.data()! as Map<String, dynamic>;
+//                         return data["seller_name"];
+//                       })
+//                       .toList()
+//                       .cast<String>(),
+//                   showSelectedItems: true,
+//                   showSearchBox: true,
+//                   onChanged: print,
+//                 );
 
-          return Container();
-        });
-  }
-}
+//           return Container();
+//         });
+//   }
+// }
