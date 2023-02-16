69b84200d830f5cf9ba04e7fb0f636ce41af728c
Nur Najihah <nurnajihah.mrizal@petronas.com>
2023-02-16 23:15:10 +0800

    Create screen for new seller (without record)

--- lib/Screens/no_search_result_screen.dart
+++ lib/Screens/no_search_result_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:trustmeter/Screens/add_seller_screen.dart';
 
 class NoSearchResultScreen extends StatefulWidget {
   const NoSearchResultScreen({super.key});
@@ -13,11 +14,58 @@ class _NoSearchResultScreenState extends State<NoSearchResultScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [Center(child: Text('no data'))],
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
+      appBar: AppBar(
+        title: Text(
+          'Search Seller',
+          style: TextStyle(fontSize: 24, color: Colors.black),
+        ),
+        backgroundColor: const Color.fromARGB(255, 135, 176, 255),
       ),
+      body: Center(
+          child: Column(
+        children: <Widget>[
+          SizedBox(
+            height: 60,
+          ),
+          Text('Trustworthiness Result:',
+              style: TextStyle(
+                fontSize: 22,
+              )),
+          SizedBox(
+            height: 40,
+          ),
+          Text('Seller Name',
+              style: TextStyle(
+                fontSize: 18,
+              )),
+          SizedBox(
+            height: 100,
+          ),
+          Text('No Result Found',
+              style: TextStyle(
+                fontSize: 20,
+              )),
+          SizedBox(
+            height: 100,
+          ),
+          TextButton(
+            child: Container(
+              color: const Color.fromARGB(255, 43, 115, 255),
+              padding: const EdgeInsets.symmetric(vertical: 13, horizontal: 20),
+              child: const Text(
+                'Create New Seller',
+                style: TextStyle(fontSize: 16.0, color: Colors.white),
+              ),
+            ),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => AddSeller()),
+              );
+            },
+          ),
+        ],
+      )),
     );
   }
 }
