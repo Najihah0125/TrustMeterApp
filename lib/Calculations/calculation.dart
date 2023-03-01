040ef515ec1e3564cd240380e6e030b48f33234c
Nur Najihah <nurnajihah.mrizal@petronas.com>
2023-03-01 23:42:41 +0800

    Matching user input with 4 big categories rules and final trust rules

--- lib/Calculations/calculation.dart
+++ lib/Calculations/calculation.dart
@@ -1,4 +1,5 @@
 import 'package:trustmeter/Screens/evaluate_criteria_screen.dart';
+import 'package:collection/collection.dart';
 
 class Calculations {
   List<String> rates = [];
@@ -6,10 +7,14 @@ class Calculations {
   List<String> scc = [];
   List<String> iq = [];
   List<String> people = [];
+  List<String> allCategoriesFinal = [];
   String ewomFinal = '';
   String sccFinal = '';
   String iqFinal = '';
   String peopleFinal = '';
+  String trustResult = '';
+  final unOrdDeepEq = const DeepCollectionEquality.unordered().equals;
+  var not = ['not', 'not', 'not'];
 
   void assignRate(List<double> criteria) {
     rates = criteria.map((criteria) {
@@ -28,16 +33,13 @@ class Calculations {
     scc = rates.sublist(3, 5);
     iq = rates.sublist(5, 9);
     people = rates.sublist(9, 12);
-
     print(rates);
-    print(ewom);
-    print(scc);
-    print(iq);
-    print(people);
-    finalRate();
+
+    finalRateFourCategories();
   }
 
-  void finalRate() {
+  void finalRateFourCategories() {
+    //for ewom & people
     var lookupThreeElements = {
       ['high', 'high', 'high']: 'high',
       ['high', 'high', 'moderate']: 'high',
@@ -68,6 +70,7 @@ class Calculations {
       ['low', 'low', 'low']: 'low',
     };
 
+    //for scc
     var lookupTwoElements = {
       ['high', 'high']: 'high',
       ['high', 'moderate']: 'high',
@@ -80,6 +83,7 @@ class Calculations {
       ['low', 'low']: 'low',
     };
 
+    //for iq
     var lookupFourElements = {
       ['high', 'high', 'high', 'high']: 'high',
       ['high', 'high', 'high', 'moderate']: 'high',
@@ -166,14 +170,166 @@ class Calculations {
       ['low', 'low', 'low', 'low']: 'low',
     };
 
-    ewomFinal = lookupThreeElements[[ewom[0], ewom[1], ewom[2]]]!;
-    sccFinal = lookupTwoElements[[scc[0], scc[1]]]!;
-    iqFinal = lookupFourElements[[iq[0], iq[1], iq[2], iq[3]]]!;
-    peopleFinal = lookupThreeElements[[people[0], people[1], people[2]]]!;
+    //matching ewom with lookup
+    var foundKeyForEwom = lookupThreeElements.keys.firstWhere(
+      (key) => unOrdDeepEq(key, ewom),
+      orElse: () => not,
+    );
 
-    print('Ewom Final' + ewomFinal);
-    print('Scc Final' + sccFinal);
-    print('Iq Final' + iqFinal);
-    print('people Final' + peopleFinal);
+    if (foundKeyForEwom != null) {
+      var value = lookupThreeElements[foundKeyForEwom];
+      ewomFinal = value ?? 'ewom invalid';
+      print('The value for key $ewom is $ewomFinal');
+    } else {
+      print('Key $ewom not found in lookup');
+    }
+
+    //matching scc with lookup
+    var foundKeyForScc = lookupTwoElements.keys.firstWhere(
+      (key) => unOrdDeepEq(key, scc),
+      orElse: () => not,
+    );
+
+    if (foundKeyForScc != null) {
+      var value = lookupTwoElements[foundKeyForScc];
+      sccFinal = value ?? 'scc invalid';
+      print('The value for key $scc is $sccFinal');
+    } else {
+      print('Key $scc not found in lookup');
+    }
+
+    //matching iq with lookup
+    var foundKeyForIq = lookupFourElements.keys.firstWhere(
+      (key) => unOrdDeepEq(key, iq),
+      orElse: () => not,
+    );
+
+    if (foundKeyForIq != null) {
+      var value = lookupFourElements[foundKeyForIq];
+      iqFinal = value ?? 'iq not valid';
+      print('The value for key $iq is $iqFinal');
+    } else {
+      print('Key $iq not found in lookup');
+    }
+
+    //matching people with lookup
+    var foundKeyForPeople = lookupThreeElements.keys.firstWhere(
+      (key) => unOrdDeepEq(key, people),
+      orElse: () => not,
+    );
+
+    if (foundKeyForPeople != null) {
+      var value = lookupThreeElements[foundKeyForPeople];
+      peopleFinal = value ?? 'people not valid';
+      print('The value for key $people is $peopleFinal');
+    } else {
+      print('Key $people not found in lookup');
+    }
+
+    allCategoriesFinal = [ewomFinal, sccFinal, iqFinal, peopleFinal];
+    print('allcategoriesfinal: $allCategoriesFinal');
+    finalTrust();
+  }
+
+  void finalTrust() {
+    var lookupFinalTrust = {
+      ['high', 'high', 'high', 'high']: 'high',
+      ['high', 'high', 'high', 'moderate']: 'moderate',
+      ['high', 'high', 'high', 'low']: 'high',
+      ['high', 'high', 'moderate', 'high']: 'high',
+      ['high', 'high', 'moderate', 'moderate']: 'moderate',
+      ['high', 'high', 'moderate', 'low']: 'moderate',
+      ['high', 'high', 'low', 'high']: 'high',
+      ['high', 'high', 'low', 'moderate']: 'moderate',
+      ['high', 'high', 'low', 'low']: 'moderate',
+      ['high', 'moderate', 'high', 'high']: 'high',
+      ['high', 'moderate', 'high', 'moderate']: 'moderate',
+      ['high', 'moderate', 'high', 'low']: 'moderate',
+      ['high', 'moderate', 'moderate', 'high']: 'high',
+      ['high', 'moderate', 'moderate', 'moderate']: 'moderate',
+      ['high', 'moderate', 'moderate', 'low']: 'moderate',
+      ['high', 'moderate', 'low', 'high']: 'moderate',
+      ['high', 'moderate', 'low', 'moderate']: 'moderate',
+      ['high', 'moderate', 'low', 'low']: 'low',
+      ['high', 'low', 'high', 'high']: 'moderate',
+      ['high', 'low', 'high', 'moderate']: 'moderate',
+      ['high', 'low', 'high', 'low']: 'moderate',
+      ['high', 'low', 'moderate', 'high']: 'moderate',
+      ['high', 'low', 'moderate', 'moderate']: 'moderate',
+      ['high', 'low', 'moderate', 'low']: 'low',
+      ['high', 'low', 'low', 'high']: 'moderate',
+      ['high', 'low', 'low', 'moderate']: 'low',
+      ['high', 'low', 'low', 'low']: 'low',
+      //moderate
+      ['moderate', 'high', 'high', 'high']: 'high',
+      ['moderate', 'high', 'high', 'moderate']: 'moderate',
+      ['moderate', 'high', 'high', 'low']: 'moderate',
+      ['moderate', 'high', 'moderate', 'high']: 'high',
+      ['moderate', 'high', 'moderate', 'moderate']: 'moderate',
+      ['moderate', 'high', 'moderate', 'low']: 'moderate',
+      ['moderate', 'high', 'low', 'high']: 'moderate',
+      ['moderate', 'high', 'low', 'moderate']: 'moderate',
+      ['moderate', 'high', 'low', 'low']: 'low',
+      ['moderate', 'moderate', 'high', 'high']: 'moderate',
+      ['moderate', 'moderate', 'high', 'moderate']: 'moderate',
+      ['moderate', 'moderate', 'high', 'low']: 'moderate',
+      ['moderate', 'moderate', 'moderate', 'high']: 'moderate',
+      ['moderate', 'moderate', 'moderate', 'moderate']: 'moderate',
+      ['moderate', 'moderate', 'moderate', 'low']: 'low',
+      ['moderate', 'moderate', 'low', 'high']: 'moderate',
+      ['moderate', 'moderate', 'low', 'moderate']: 'low',
+      ['moderate', 'moderate', 'low', 'low']: 'low',
+      ['moderate', 'low', 'high', 'high']: 'moderate',
+      ['moderate', 'low', 'high', 'moderate']: 'moderate',
+      ['moderate', 'low', 'high', 'low']: 'low',
+      ['moderate', 'low', 'moderate', 'high']: 'moderate',
+      ['moderate', 'low', 'moderate', 'moderate']: 'moderate',
+      ['moderate', 'low', 'moderate', 'low']: 'low',
+      ['moderate', 'low', 'low', 'high']: 'low',
+      ['moderate', 'low', 'low', 'moderate']: 'low',
+      ['moderate', 'low', 'low', 'low']: 'low',
+      //low
+      ['low', 'high', 'high', 'high']: 'moderate',
+      ['low', 'high', 'high', 'moderate']: 'moderate',
+      ['low', 'high', 'high', 'low']: 'moderate',
+      ['low', 'high', 'moderate', 'high']: 'moderate',
+      ['low', 'high', 'moderate', 'moderate']: 'moderate',
+      ['low', 'high', 'moderate', 'low']: 'low',
+      ['low', 'high', 'low', 'high']: 'moderate',
+      ['low', 'high', 'low', 'moderate']: 'low',
+      ['low', 'high', 'low', 'low']: 'low',
+      ['low', 'moderate', 'high', 'high']: 'moderate',
+      ['low', 'moderate', 'high', 'moderate']: 'moderate',
+      ['low', 'moderate', 'high', 'low']: 'low',
+      ['low', 'moderate', 'moderate', 'high']: 'moderate',
+      ['low', 'moderate', 'moderate', 'moderate']: 'moderate',
+      ['low', 'moderate', 'moderate', 'low']: 'low',
+      ['low', 'moderate', 'low', 'high']: 'low',
+      ['low', 'moderate', 'low', 'moderate']: 'low',
+      ['low', 'moderate', 'low', 'low']: 'moderate',
+      ['low', 'low', 'high', 'high']: 'moderate',
+      ['low', 'low', 'high', 'moderate']: 'low',
+      ['low', 'low', 'high', 'low']: 'low',
+      ['low', 'low', 'moderate', 'high']: 'low',
+      ['low', 'low', 'moderate', 'moderate']: 'low',
+      ['low', 'low', 'moderate', 'low']: 'moderate',
+      ['low', 'low', 'low', 'high']: 'low',
+      ['low', 'low', 'low', 'moderate']: 'moderate',
+      ['low', 'low', 'low', 'low']: 'low',
+    };
+
+    //matching finalTrust with lookup
+    var foundKeyForFinalTrust = lookupFinalTrust.keys.firstWhere(
+      (key) => unOrdDeepEq(key, allCategoriesFinal),
+      orElse: () => not,
+    );
+
+    if (foundKeyForFinalTrust != null) {
+      var value = lookupFinalTrust[foundKeyForFinalTrust];
+      trustResult = value ?? 'people not valid';
+      print('The value for key $allCategoriesFinal is $trustResult');
+    } else {
+      print('Key $allCategoriesFinal not found in lookup');
+    }
   }
 }
