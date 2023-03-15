30056700bfae0b375c03cbaf11ac3b9ce8c36271
Nur Najihah <nurnajihah.mrizal@petronas.com>
2023-03-15 11:58:03 +0800

    Display list of evaluations

--- lib/Screens/has_search_result_screen.dart
+++ lib/Screens/has_search_result_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:trustmeter/Screens/evaluate_criteria_screen.dart';
+import 'package:intl/intl.dart';
 
 class HasSearchResultScreen extends StatefulWidget {
   final String? sellerName;
@@ -78,12 +79,29 @@ class _HasSearchResultScreenState extends State<HasSearchResultScreen> {
                           itemCount: items.length,
                           itemBuilder: (context, index) {
                             final item = items[index];
+                            //change user email format
+                            String userEmail = item['user_email'];
+                            String shortEmail = userEmail.split('@')[0];
+                            String encryptEmail = shortEmail.replaceRange(
+                                1,
+                                shortEmail.length - 1,
+                                '*' * (shortEmail.length - 2));
+                            //get trust result in string
+                            String trustResult = items.last.get('trust_result');
                             return Container(
                               margin: EdgeInsets.fromLTRB(30, 10, 30, 10),
                               child: Card(
                                   child: ListTile(
-                                      leading: Icon(Icons.person),
-                                      title: Text(item['trust_result']))),
+                                leading: Icon(
+                                  Icons.person,
+                                  size: 40,
+                                ),
+                                title: Text(encryptEmail),
+                                trailing: starsResult(trustResult),
+                                subtitle: Text(DateFormat('yyyy-MM-dd').format(
+                                    (item['date_created'] as Timestamp)
+                                        .toDate())),
+                              )),
                             );
                           },
                         ),
@@ -135,6 +153,39 @@ class _HasSearchResultScreenState extends State<HasSearchResultScreen> {
     }
   }
 
-  //Widget method to display list of evaluations
-  // Widget listEvaluations(String? sellerName, String? typeAcc) {}
+  //widget method to create stars rating of trust result
+  Widget starsResult(String trustResult) {
+    if (trustResult == 'high') {
+      return Row(
+        mainAxisSize: MainAxisSize.min,
+        children: List.generate(
+            3,
+            (index) => Icon(Icons.star,
+                color: Color.fromARGB(255, 255, 206, 9), size: 20)),
+      );
+    } else if (trustResult == 'moderate') {
+      return Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Icon(Icons.star, color: Color.fromARGB(255, 255, 206, 9), size: 20),
+          Icon(Icons.star, color: Color.fromARGB(255, 255, 206, 9), size: 20),
+          Icon(Icons.star_border,
+              color: Color.fromARGB(255, 255, 206, 9), size: 20),
+        ],
+      );
+    } else if (trustResult == 'low') {
+      return Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Icon(Icons.star, color: Color.fromARGB(255, 255, 206, 9), size: 20),
+          Icon(Icons.star_border,
+              color: Color.fromARGB(255, 255, 206, 9), size: 20),
+          Icon(Icons.star_border,
+              color: Color.fromARGB(255, 255, 206, 9), size: 20),
+        ],
+      );
+    } else {
+      return Text('invalid');
+    }
+  }
 }
