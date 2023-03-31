f7cf3f846659362fc84ee4a63584d13152899efe
Nur Najihah <nurnajihah.mrizal@petronas.com>
2023-03-31 12:15:10 +0800

    Delete unused import

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:trustmeter/Screens/auth_screen.dart';
 import 'package:trustmeter/Screens/home_screen.dart';
-import 'package:trustmeter/Screens/welcome_screen.dart';
 import 'Screens/utils.dart';
 
 Future main() async {
